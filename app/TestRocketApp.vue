fceb38457a5b899c383dad2216a8e17840d5c7d2
Wesley Klop <wesley19097@gmail.com>
2020-03-03 14:02:08 +0100

    Add prettier script and format code

--- app/TestRocketApp.vue
+++ app/TestRocketApp.vue
@@ -2,30 +2,30 @@
     <div class="p-4">
         <h1 class="text-xl font-bold">Users lijst</h1>
         <ul>
-            <li :key="user.email" v-for="user of users">{{ user.full_name }}</li>
+            <li :key="user.email" v-for="user of users">
+                {{ user.full_name }}
+            </li>
         </ul>
     </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                users: [],
-            }
-        },
-        async mounted() {
-            const result = await fetch('/api/user', {
-                headers: {
-                    Accept: 'application/json',
-                },
-            })
+export default {
+    data() {
+        return {
+            users: [],
+        }
+    },
+    async mounted() {
+        const result = await fetch('/api/user', {
+            headers: {
+                Accept: 'application/json',
+            },
+        })
 
-            this.users = await result.json()
-        },
-    }
+        this.users = await result.json()
+    },
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
