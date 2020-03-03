73987eeb93131b47e0d3862f7fe5f665901b49ec
Wesley Klop <wesley19097@gmail.com>
2020-03-03 13:36:52 +0100

    Show user list via vue, create dockerfile for frontend

--- app/TestRocketApp.vue
+++ app/TestRocketApp.vue
@@ -1,10 +1,28 @@
 <template>
-    <div class="bg-blue-500">Hey daar</div>
+    <div class="p-4">
+        <h1 class="text-xl font-bold">Users lijst</h1>
+        <ul>
+            <li :key="user.email" v-for="user of users">{{ user.full_name }}</li>
+        </ul>
+    </div>
 </template>
 
 <script>
     export default {
-        name: 'TestRocketApp',
+        data() {
+            return {
+                users: [],
+            }
+        },
+        async mounted() {
+            const result = await fetch('/api/user', {
+                headers: {
+                    Accept: 'application/json',
+                },
+            })
+
+            this.users = await result.json()
+        },
     }
 </script>
 
