4dff05485d80ddb99d6f940cdbdf52338c744d17
Wesley Klop <wesley19097@gmail.com>
2020-03-03 12:30:27 +0100

    Setup production grade css building

--- webpack.config.js
+++ webpack.config.js
@@ -2,61 +2,83 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const { resolve } = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-    entry: './app/index.js',
-    context: __dirname,
-    output: {
-        path: resolve(__dirname, 'public'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader',
-            },
 
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                options: {
-                    presets: ['@babel/preset-env'],
-                    plugins: [
-                        '@babel/plugin-transform-runtime',
+const getFinalCssLoader = mode => {
+    if (mode === 'production') {
+        return MiniCssExtractPlugin.loader
+    }
+    return 'vue-style-loader'
+}
+
+module.exports = (env, { mode }) => {
+    const config = ({
+        entry: './app/index.js',
+        context: __dirname,
+        output: {
+            path: resolve(__dirname, 'public'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.vue$/,
+                    loader: 'vue-loader',
+                },
+
+                {
+                    test: /\.js$/,
+                    loader: 'babel-loader',
+                    exclude: /node_modules/,
+                    options: {
+                        presets: [['@babel/preset-env', {
+                            modules: false,
+                            'targets': {
+                                'browsers': ['> 1%', 'last 2 versions', 'not ie <= 8'],
+                            },
+                        }]],
+                        plugins: [
+                            '@babel/plugin-transform-runtime',
+                        ],
+                    },
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        getFinalCssLoader(mode),
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                importLoaders: 1,
+                            },
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                plugins: [
+                                    require('tailwindcss'),
+                                ],
+                            },
+                        },
                     ],
                 },
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'vue-style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            importLoaders: 1,
-                        },
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: [
-                                require('tailwindcss'),
-                            ],
-                        },
-                    },
-                ],
-            },
+            ],
+        },
+        resolve: {
+            extensions: ['.vue', '.css', '.wasm', '.mjs', '.js', '.json'],
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new VueLoaderPlugin(),
+            new HtmlWebpackPlugin({
+                title: 'Test rocket app',
+            }),
         ],
-    },
-    resolve: {
-        extensions: ['.vue', '.css', '.wasm', '.mjs', '.js', '.json'],
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new VueLoaderPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'Test rocket app',
-        }),
-    ],
+    })
+
+    if (mode === 'production') {
+        config.plugins.push(new MiniCssExtractPlugin({}))
+    }
+
+    return config
 }
